2e37289e38a991a2b30102616fca97ae68d57ff2
malihu <manos@malihu.gr>
2017-06-19 21:05:21 +0300

    yt js api updates

--- js/main.js
+++ js/main.js
@@ -14,23 +14,29 @@ var player;
 })(jQuery);
 
 function onYouTubeIframeAPIReady() {
-	console.log( YT.loaded );
-	player = new YT.Player('video-player', {
-		width: 600,
-		height: 400,
-		videoId: 'To5oBOMAte8',
-		playerVars: {
-			color: 'white',
-			showinfo: 0,
-			autoplay: 1,
-			rel: 0,
-			playlist: '0zsh2Qx2bbQ,sAspme4mzfg,Nln_ONLzehI,TF_32PYY-RM,1MgQ7K_1Fng,W_lDC3VmgJs,EFvLvmyv1iM,ohSMsnz0Qe4,RKFoT6dVRiA,HuBO4QdZPmY,fEUAsuF2XkY,fFs_SS6LcEo,T5EXH-r0738,Nfy18lvADOA,1LZeV1gAizg,HzjOogDeZss'
-		},
-		events: {
-			//onReady: initialize,
-			onStateChange: onPlayerStateChange
+	//console.log( YT.loaded );
+	var checkYT = setInterval(function () {
+		console.log( "L: "+YT.loaded );
+    	if(YT.loaded){
+			player = new YT.Player('video-player', {
+				width: 600,
+				height: 400,
+				videoId: 'To5oBOMAte8',
+				playerVars: {
+					color: 'white',
+					showinfo: 0,
+					autoplay: 1,
+					rel: 0,
+					playlist: '0zsh2Qx2bbQ,sAspme4mzfg,Nln_ONLzehI,TF_32PYY-RM,1MgQ7K_1Fng,W_lDC3VmgJs,EFvLvmyv1iM,ohSMsnz0Qe4,RKFoT6dVRiA,HuBO4QdZPmY,fEUAsuF2XkY,fFs_SS6LcEo,T5EXH-r0738,Nfy18lvADOA,1LZeV1gAizg,HzjOogDeZss'
+				},
+				events: {
+					//onReady: initialize,
+					onStateChange: onPlayerStateChange
+				}
+			});
+			clearInterval(checkYT);
 		}
-	});
+	},100);
 }
 
 function onPlayerStateChange(event) {
